591c1301534d9655f2aa18da17fa19e7494acb3c
mariananieton <rm94141@fiap.com.br>
2023-01-26 13:59:05 -0300

    Adicionando funcionalidade de listagem de paciente + ordenacao + paginacao

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -3,14 +3,15 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -23,6 +24,11 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 
 }
