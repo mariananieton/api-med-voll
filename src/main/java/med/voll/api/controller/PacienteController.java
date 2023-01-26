e0be150ea248dcd6f101b7e0ef412d718e8a7ffc
mariananieton <rm94141@fiap.com.br>
2023-01-26 13:20:18 -0300

    Criando tabela pacientes + adicionando PacienteRepository + adicionando validacoes aos DadosCadastroPaciente + criando e mapeando entidade Paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,6 +2,10 @@ package med.voll.api.controller;
 
 
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
 
+    @Autowired
+    PacienteRepository repository;
+
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println("Dados recebidos :" + dados);
+        repository.save(new Paciente(dados));
     }
 
 }
